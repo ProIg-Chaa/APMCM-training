d872059d981581f0fdf7fdf9aa33202b7bf1fb05
ProIg-Chaa <1156364963@qq.com>
2025-10-30 12:42:03 +0800

    env

---
+++ setup.bat
@@ -0,0 +1,13 @@
+@echo off  
+echo What is doing for is to create a conda environment 
+
+set /p choice="Do you want to create a conda environment? (y/n): "
+if /i "%choice%"=="y" (
+    echo Creating conda environment...
+    call conda env create -f environment.yml
+    
+) else (
+    echo Skipping conda environment creation.
+)
+pause 
+    
